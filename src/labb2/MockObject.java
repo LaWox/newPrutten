ed04d980fe785392ec3728ea86e0fc90ef6e1b2c
J Kangas <jussi.v.kangas@gmail.com>
2018-11-05 13:39:13 +0100

    mockobject

---
+++ src/labb2/MockObject.java
@@ -0,0 +1,44 @@
+package labb2;
+
+public class MockObject implements Boardgame {
+
+    private Board board;
+    private int size;
+
+    MockObject(int size){
+        this.board=new Board(size);
+        this.board.setUp();
+        this.size=size;
+
+    }
+
+    public boolean move(int i, int j) {
+        try {
+            this.board[i][j]
+            this.getMessage();
+            System.out.println("The move was a succes!")
+            return true;
+        }
+        catch (IndexOutOfBoundsException e) {
+            this.getMessage();
+            System.out.println("Index error");
+            return false;
+         }
+     }
+
+
+    public String getStatus(int i, int j) {
+        try{
+            return String.valueOf(this.board.board_matrix[i][j].value);
+        }
+
+        catch(IndexOutOfBoundsException e){
+             return null;
+        }
+    }
+
+    public String getMessage(){
+        System.out.println("Your move was processed");
+
+    }
+}
